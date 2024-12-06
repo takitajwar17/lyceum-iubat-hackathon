2f30ec06f641170975ff5eb905c3a2eed6343031
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-12-06 09:45:33 +0600

    Add dashboard

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+const Dashboard = () => {
+  return (
+    <>
+      <div className="min-h-full min-w-full">
+        <h1 className="text-2xl font-bold mb-5 text-black">Dashboard</h1>
+        <p className="mb-5 text-black">Welcome to the dashboard!</p>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
