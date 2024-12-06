b7d5a6d6dff8cefe5017e653500494d044cf9e76
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-12-06 11:31:26 +0600

    [videoId]: sections resizeable

--- app/learn/[videoId]/page.jsx
+++ app/learn/[videoId]/page.jsx
@@ -4,10 +4,37 @@ import React, { useEffect } from "react";
 const VideoPage = ({ params }) => {
   const { videoId } = params;
 
+  useEffect(() => {
+    const resizer = document.querySelector(".cursor-ew-resize");
+    const leftPanel = resizer.previousElementSibling;
+    const rightPanel = resizer.nextElementSibling;
+
+    const handleMouseDown = (event) => {
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+    };
+
+    const handleMouseMove = (event) => {
+      const newWidth = event.clientX - leftPanel.getBoundingClientRect().left;
+      leftPanel.style.width = `${newWidth}px`;
+      rightPanel.style.width = `calc(100% - ${newWidth}px - 0.25rem)`; // Adjust for the width of the resizer (0.25rem = 1px)
+    };
+
+    const handleMouseUp = () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    resizer.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      resizer.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
 
   return (
     <div className="flex bg-gray-100">
-      <div className="w-1/2 h-[calc(80vh-64px)] overflow-hidden bg-gray-100">
+      <div className="resizeable w-1/2 h-[calc(80vh-64px)] overflow-hidden bg-gray-100">
         {/* Embed YouTube Video */}
         <iframe
           width="100%"
