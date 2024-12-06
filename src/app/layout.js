6a735b07d71c85d2f089943edb082edf14310bd9
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-12-06 09:07:52 +0600

    font added: Kanit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,19 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Kanit } from "next/font/google";
+
+const kanit = Kanit({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -21,7 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kanit.className} antialiased`}
       >
         {children}
       </body>
